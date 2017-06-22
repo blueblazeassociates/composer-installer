dc422e194809dbf61b1d4c98df01489afe7f74af
Ed Gifford <egifford@corexcel.com>
2017-06-22 16:50:08 -0400

    Adding logic to look for preferred php executable and adding -d allow_url_fopen=1

--- composer-install.bash
+++ composer-install.bash
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
-curl -sS https://getcomposer.org/installer | php-cli
+if [ ! -z "$COMPOSER_CLI_PHP" ] ; then
+	# Use the COMPOSER_CLI_PHP environment variable if it is available.
+	php="$COMPOSER_CLI_PHP"
+else
+	# Default to using the php that we find on the PATH.
+	# Note that we need the full path to php here for Dreamhost, which behaves oddly.  See http://drupal.org/node/662926
+	php="`which php`"
+fi
+
+curl -sS https://getcomposer.org/installer | "$php" -d allow_url_fopen=1
